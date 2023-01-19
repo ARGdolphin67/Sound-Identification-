c87d9ff01e3fa6a49a79f7448524ea3a2de8616a
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2023-01-19 11:18:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //https://teachablemachine.withgoogle.com/models/oN9BbutVo/
+
+//https://teachablemachine.withgoogle.com/models/VNl-mLZFg/ animal
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/oN9BbutVo/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classifier(gotResults);
+}
