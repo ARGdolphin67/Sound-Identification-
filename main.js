a6656f9c89951c209fbb3bd5c37c7deae1ae229f
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2023-01-23 11:47:25 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function startClassification(){
     navigator.mediaDevices.getUserMedia({
         audio:true
     });
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/oN9BbutVo/model.json',modelReady);
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/VNl-mLZFg/model.json',modelReady);
 }
 
 function modelReady(){
